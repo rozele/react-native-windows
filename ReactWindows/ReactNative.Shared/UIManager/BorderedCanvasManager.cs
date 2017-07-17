898cccb6165e8139f1016790ce0bafea0422a357
Anton Khayrudinov <antonkh@microsoft.com>
2017-07-17 15:33:58 -0700

    Replaced XAML bindings with SetDimensions

--- ReactWindows/ReactNative.Shared/UIManager/BorderedCanvasManager.cs
+++ ReactWindows/ReactNative.Shared/UIManager/BorderedCanvasManager.cs
@@ -15,39 +15,52 @@ namespace ReactNative.UIManager
     public abstract class BorderedCanvasManager<TCanvas> : BorderedViewParentManager<TCanvas>
         where TCanvas : Canvas
     {
+        /// <summary>
+        /// Checks if the Canvas has a Border already.
+        /// </summary>
         protected override bool HasBorder(TCanvas view)
         {
             return GetBorder(view) != null;
         }
 
+        /// <summary>
+        /// Gets a Border associated with this Canvas or null if it doesn't have a border.
+        /// </summary>
         private Border GetBorder(TCanvas view)
         {
             return view.Children[0] as Border;
         }
 
+        /// <summary>
+        /// Adds a Border to a Canvas if it hasn't been added already.
+        /// </summary>
         protected override Border GetOrCreateBorder(TCanvas view)
         {
             var border = GetBorder(view);
 
-            if (border == null) // check Tag here?
+            if (border == null)
             {
-                border = new Border { BorderBrush = s_defaultBorderBrush, Tag = 123 };
+                border = new Border { BorderBrush = s_defaultBorderBrush };
                 view.Children.Insert(0, border);
-
-                border.SetBinding(FrameworkElement.WidthProperty, new Binding
-                {
-                    Source = view,
-                    Path = new PropertyPath("Width")
-                });
-
-                border.SetBinding(FrameworkElement.HeightProperty, new Binding
-                {
-                    Source = view,
-                    Path = new PropertyPath("Height")
-                });
             }
 
             return border;
+        }
+
+        /// <summary>
+        /// Keeps the Border's dimensions in sync with with parent Canvas.
+        /// </summary>
+        public override void SetDimensions(TCanvas view, Dimensions dimensions)
+        {
+            base.SetDimensions(view, dimensions);
+
+            var border = GetBorder(view);
+
+            if (border != null)
+            {
+                border.Width = dimensions.Width;
+                border.Height = dimensions.Height;
+            }
         }
 
         /// <summary>
