8ba79f08f2d1df742314a2731237b8c21f173330
Anton Khayrudinov <antonkh@microsoft.com>
2017-07-31 14:08:28 -0700

    Inherit ReactViewManager from ViewParentManager directly

--- ReactWindows/ReactNative.Shared/Views/View/ReactViewManager.cs
+++ ReactWindows/ReactNative.Shared/Views/View/ReactViewManager.cs
@@ -1,12 +1,16 @@
 ﻿using ReactNative.Reflection;
 using ReactNative.UIManager;
 using ReactNative.UIManager.Annotations;
-using Windows.UI.Xaml;
 #if WINDOWS_UWP
+using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
 #else
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 #endif
 
 namespace ReactNative.Views.View
@@ -14,8 +18,22 @@ namespace ReactNative.Views.View
     /// <summary>
     /// View manager for React view instances.
     /// </summary>
-    public class ReactViewManager : BorderedCanvasManager<BorderedCanvas>
+    public class ReactViewManager : ViewParentManager<BorderedCanvas>
     {
+        private enum Radius
+        {
+            All,
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight,
+        }
+
+        /// <summary>
+        /// Default brush for the view borders.
+        /// </summary>
+        protected static readonly Brush s_defaultBorderBrush = new SolidColorBrush(Colors.Black);
+
         /// <summary>
         /// The name of this view manager. This will be the name used to 
         /// reference this view manager from JavaScript.
@@ -31,7 +49,7 @@ namespace ReactNative.Views.View
         /// <summary>
         /// Checks if the Canvas has a Border already.
         /// </summary>
-        protected override bool HasBorder(BorderedCanvas view)
+        protected bool HasBorder(BorderedCanvas view)
         {
             return view.Border != null;
         }
@@ -39,7 +57,7 @@ namespace ReactNative.Views.View
         /// <summary>
         /// Adds a Border to a Canvas if it hasn't been added already.
         /// </summary>
-        protected override Border GetOrCreateBorder(BorderedCanvas view)
+        protected Border GetOrCreateBorder(BorderedCanvas view)
         {
             if (view.Border == null)
             {
@@ -101,6 +119,92 @@ namespace ReactNative.Views.View
         }
 
         /// <summary>
+        /// Sets the border radius of the view.
+        /// </summary>
+        /// <param name="view">The view panel.</param>
+        /// <param name="index">The property index.</param>
+        /// <param name="radius">The border radius value.</param>
+        [ReactPropGroup(
+            ViewProps.BorderRadius,
+            ViewProps.BorderTopLeftRadius,
+            ViewProps.BorderTopRightRadius,
+            ViewProps.BorderBottomLeftRadius,
+            ViewProps.BorderBottomRightRadius)]
+        public void SetBorderRadius(BorderedCanvas view, int index, double radius)
+        {
+            var border = GetOrCreateBorder(view);
+            var cornerRadius = border.CornerRadius == null ? new CornerRadius() : border.CornerRadius;
+
+            switch ((Radius)index)
+            {
+                case Radius.All:
+                    cornerRadius = new CornerRadius(radius);
+                    break;
+                case Radius.TopLeft:
+                    cornerRadius.TopLeft = radius;
+                    break;
+                case Radius.TopRight:
+                    cornerRadius.TopRight = radius;
+                    break;
+                case Radius.BottomLeft:
+                    cornerRadius.BottomLeft = radius;
+                    break;
+                case Radius.BottomRight:
+                    cornerRadius.BottomRight = radius;
+                    break;
+            }
+
+            border.CornerRadius = cornerRadius;
+        }
+
+        /// <summary>
+        /// Sets the background color of the view.
+        /// </summary>
+        /// <param name="view">The view panel.</param>
+        /// <param name="color">The masked color value.</param>
+        [ReactProp(
+            ViewProps.BackgroundColor,
+            CustomType = "Color",
+            DefaultUInt32 = ColorHelpers.Transparent)]
+        public void SetBackgroundColor(BorderedCanvas view, uint color)
+        {
+            var border = GetOrCreateBorder(view);
+            border.Background = new SolidColorBrush(ColorHelpers.Parse(color));
+        }
+
+        /// <summary>
+        /// Set the border color of the view.
+        /// </summary>
+        /// <param name="view">The view panel.</param>
+        /// <param name="color">The color hex code.</param>
+        [ReactProp("borderColor", CustomType = "Color")]
+        public void SetBorderColor(BorderedCanvas view, uint? color)
+        {
+            var border = GetOrCreateBorder(view);
+            border.BorderBrush = color.HasValue
+                ? new SolidColorBrush(ColorHelpers.Parse(color.Value))
+                : s_defaultBorderBrush;
+        }
+
+        /// <summary>
+        /// Sets the border thickness of the view.
+        /// </summary>
+        /// <param name="view">The view panel.</param>
+        /// <param name="index">The property index.</param>
+        /// <param name="width">The border width in pixels.</param>
+        [ReactPropGroup(
+            ViewProps.BorderWidth,
+            ViewProps.BorderLeftWidth,
+            ViewProps.BorderRightWidth,
+            ViewProps.BorderTopWidth,
+            ViewProps.BorderBottomWidth)]
+        public void SetBorderWidth(BorderedCanvas view, int index, double width)
+        {
+            var border = GetOrCreateBorder(view);
+            border.SetBorderWidth(ViewProps.BorderSpacingTypes[index], width);
+        }
+
+        /// <summary>
         /// Sets whether the view is collapsible.
         /// </summary>
         /// <param name="view">The view instance.</param>
@@ -110,6 +214,90 @@ namespace ReactNative.Views.View
         {
             // no-op: it's here only so that "collapsable" property is exported to JS. The value is actually
             // handled in NativeViewHierarchyOptimizer
+        }
+
+        /// <summary>
+        /// Adds a child at the given index.
+        /// </summary>
+        /// <param name="parent">The parent view.</param>
+        /// <param name="child">The child view.</param>
+        /// <param name="index">The index.</param>
+        public override void AddView(BorderedCanvas parent, DependencyObject child, int index)
+        {
+            if (HasBorder(parent))
+            {
+                index++;
+            }
+
+            var uiElementChild = child.As<UIElement>();
+            parent.Children.Insert(index, uiElementChild);
+        }
+
+        /// <summary>
+        /// Gets the child at the given index.
+        /// </summary>
+        /// <param name="parent">The parent view.</param>
+        /// <param name="index">The index.</param>
+        /// <returns>The child view.</returns>
+        public override DependencyObject GetChildAt(BorderedCanvas parent, int index)
+        {
+            if (HasBorder(parent))
+            {
+                index++;
+            }
+
+            return parent.Children[index];
+        }
+
+        /// <summary>
+        /// Gets the number of children in the view parent.
+        /// </summary>
+        /// <param name="parent">The view parent.</param>
+        /// <returns>The number of children.</returns>
+        public override int GetChildCount(BorderedCanvas parent)
+        {
+            var count = parent.Children.Count;
+
+            if (HasBorder(parent))
+            {
+                count--;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Removes all children from the view parent.
+        /// </summary>
+        /// <param name="parent">The view parent.</param>
+        public override void RemoveAllChildren(BorderedCanvas parent)
+        {
+            if (HasBorder(parent))
+            {
+                for (var i = parent.Children.Count - 1; i > 0; i--)
+                {
+                    parent.Children.RemoveAt(i);
+                }
+            }
+            else
+            {
+                parent.Children.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Removes the child at the given index.
+        /// </summary>
+        /// <param name="parent">The view parent.</param>
+        /// <param name="index">The index.</param>
+        public override void RemoveChildAt(BorderedCanvas parent, int index)
+        {
+            if (HasBorder(parent))
+            {
+                index++;
+            }
+
+            parent.Children.RemoveAt(index);
         }
     }
 }
