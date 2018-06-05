c37c9c11c6bdc16f5a1bb9b0d78a06ab9bc3bcee
Eric Rozell <erozell@outlook.com>
2018-06-04 16:11:04 -1000

    Temporarily adding XamlDirectLib to repo

---
+++ ReactWindows/XamlDirectLib/XamlDirectObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.UI.Xaml.Core.Direct
+{
+    public class XamlDirectObject
+    {
+        internal object obj;
+        internal XamlDirectObject(object obj)
+        {
+            this.obj = obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<object>.Default.GetHashCode(this.obj);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is XamlDirectObject xdo)
+            {
+                return EqualityComparer<object>.Default.Equals(this.obj, xdo.obj);
+            }
+
+            return false;
+        }
+    }
+}
