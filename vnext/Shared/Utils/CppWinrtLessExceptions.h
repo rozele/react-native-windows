f03c85a9efbfe5ea864785e6b5905fad0ee01cc5
Jack Erghan <jacke@fb.com>
2020-07-14 10:34:29 -0700

    ARCHON_RNW_BUILD: After cppwinrt update, remove change to make CppWinrtLessExceptions.h build.

--- vnext/Shared/Utils/CppWinrtLessExceptions.h
+++ vnext/Shared/Utils/CppWinrtLessExceptions.h
@@ -32,8 +32,7 @@ struct lessthrow_await_adapter {
   }
 
   void await_suspend(std::experimental::coroutine_handle<> handle) const {
-    // ARCHON_RNW_BUILD: WINRT_IMPL_CoGetObjectContext not defined
-    auto context = winrt::capture<winrt::impl::IContextCallback>(WINRT_CoGetObjectContext);
+    auto context = winrt::capture<winrt::impl::IContextCallback>(WINRT_IMPL_CoGetObjectContext);
 
     async.Completed([handle, context = std::move(context)](auto const &, winrt::Windows::Foundation::AsyncStatus) {
       winrt::impl::com_callback_args args{};
