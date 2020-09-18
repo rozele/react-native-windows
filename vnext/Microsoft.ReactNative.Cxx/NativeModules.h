bad6ef3efa126e5d9611e63ff4590c2d08ca1a5d
Jack Erghan <jacke@fb.com>
2020-09-18 15:37:13 -0700

    ARCHON_RNW_JSI: remove reference from shared_ptr<...instance>

--- vnext/Microsoft.ReactNative.Cxx/NativeModules.h
+++ vnext/Microsoft.ReactNative.Cxx/NativeModules.h
@@ -161,7 +161,7 @@ static auto AsyncStoragePathProperty() {
 
 // For installing bindings directly via the C++ JSI API - not ABI-safe
 static auto JSIRuntimeInstallersProperty() {
-  static ReactPropertyId<ReactNonAbiValue<std::vector<std::function<void(facebook::jsi::Runtime &runtime, std::shared_ptr<facebook::react::Instance>& instance)>>>> jsiRuntimeInstallersProperty{L"ReactNative.JSIRuntimeInstallers"};
+  static ReactPropertyId<ReactNonAbiValue<std::vector<std::function<void(facebook::jsi::Runtime &runtime, std::shared_ptr<facebook::react::Instance> instance)>>>> jsiRuntimeInstallersProperty{L"ReactNative.JSIRuntimeInstallers"};
   return jsiRuntimeInstallersProperty;
 }
 
