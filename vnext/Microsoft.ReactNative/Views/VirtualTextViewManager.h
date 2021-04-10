27ffe369b9cd6332318147bcdaeb24e1c32f2367
Eric Rozell <erozell@outlook.com>
2021-04-10 10:19:36 -0400

    Do not skip recursion when ancestor is pressable

--- vnext/Microsoft.ReactNative/Views/VirtualTextViewManager.h
+++ vnext/Microsoft.ReactNative/Views/VirtualTextViewManager.h
@@ -27,7 +27,7 @@ struct VirtualTextShadowNode final : public ShadowNodeBase {
   void AddToPressableCount(int pressableCount);
   void SetPressable(bool isPressable);
 
-  static xaml::Documents::TextPointer HitTest(const ShadowNodeBase &node, const winrt::Point &point);
+  static xaml::Documents::TextPointer HitTest(const ShadowNodeBase &node, const winrt::Point &point, bool hasPressableParent);
 
   bool m_isPressable{false};
   int m_pressableCount{0};
