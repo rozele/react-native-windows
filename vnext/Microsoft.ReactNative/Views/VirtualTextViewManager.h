ee2e1843a15bebf2bfecc6efb2f5da23d4298233
Eric Rozell <erozell@outlook.com>
2021-05-04 09:46:04 -0400

    Applying yarn format

--- vnext/Microsoft.ReactNative/Views/VirtualTextViewManager.h
+++ vnext/Microsoft.ReactNative/Views/VirtualTextViewManager.h
@@ -27,8 +27,7 @@ struct VirtualTextShadowNode final : public ShadowNodeBase {
   void AddToPressableCount(int pressableCount);
   void SetPressable(bool isPressable);
 
-  static xaml::DependencyObject
-  HitTest(const ShadowNodeBase &node, const winrt::Point &point, bool hasPressableParent);
+  static xaml::DependencyObject HitTest(const ShadowNodeBase &node, const winrt::Point &point, bool hasPressableParent);
 
   bool m_isPressable{false};
   int m_pressableCount{0};
