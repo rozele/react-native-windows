b1be788b793451680621e56553e99e1faadd7cf7
Eric Rozell <ericroz@fb.com>
2021-06-04 10:44:19 -0400

    [rn][win] Submit content before clearing
    Summary:
    Some React Native apps may ignore the text content in the event data for
    `onSubmitEditing` and favor the current state from `onChange`. Currently
    the text is submitted _after_ the TextBox has been cleared, which means
    that when the `onSubmitEditing` will be received after the `onChange`
    event resulting from the text being cleared.
    
    This was a side-effect of emitting the `onChange` event from the `TextBox.TextChanging` event (which fires synchronously with the change to `TextBox.Text`) from what was previously used (`TextBox.TextChanged`).
    
    Test Plan:
    |Before|After|
    |{F621766533}|{F621765800}|
    
    Reviewers: skyle, lyahdav, mylando
    
    Reviewed By: skyle
    
    Subscribers: eliwhite
    
    Differential Revision: https://phabricator.intern.facebook.com/D28899660
    
    Signature: 28899660:1622818789:85634bdcbc7e9afb25915a90c8a23b1b57ab4618

--- vnext/Microsoft.ReactNative/Views/TextInputViewManager.cpp
+++ vnext/Microsoft.ReactNative/Views/TextInputViewManager.cpp
@@ -476,6 +476,8 @@ void TextInputShadowNode::registerPreviewKeyDown() {
                   "text", HstringToDynamic(control.as<xaml::Controls::PasswordBox>().Password()));
             }
 
+            instance->DispatchEvent(tag, "topTextInputSubmitEditing", std::move(eventDataSubmitEditing));
+
             if (m_shouldClearTextOnSubmit) {
               if (m_isTextBox) {
                 control.as<xaml::Controls::TextBox>().ClearValue(xaml::Controls::TextBox::TextProperty());
@@ -483,8 +485,6 @@ void TextInputShadowNode::registerPreviewKeyDown() {
                 control.as<xaml::Controls::PasswordBox>().ClearValue(xaml::Controls::PasswordBox::PasswordProperty());
               }
             }
-
-            instance->DispatchEvent(tag, "topTextInputSubmitEditing", std::move(eventDataSubmitEditing));
 
             // For multi-line TextInput, we have to mark the PreviewKeyDown event as
             // handled to prevent the TextInput from adding a newline character
