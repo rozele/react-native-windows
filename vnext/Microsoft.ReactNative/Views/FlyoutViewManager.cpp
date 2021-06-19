b44768efac228680e6e7af2d50207618e43db551
Eric Rozell <ericroz@fb.com>
2021-06-18 22:07:22 -0400

    [rn][win] Fix XamlRoot crashes in FlyoutViewManager
    Summary:
    There are two cases where a flyout may crash when opened:
    
    1. If the native view corresponding to the target tag is unmounted before the flyout is shown.
    2. If the XamlRoot on the flyout differs from the XamlRoot of the target.
    
    This can happen if the XamlRoot changes (e.g., window loses focus) before the UIManager operation to create the Flyout and set the isOpen prop executes. This change logs the error and does not attempt to show the flyout if either of these cases occur.
    
    Test Plan: See test plan for D29247782
    
    Reviewers: skyle, lyahdav, mylando
    
    Reviewed By: lyahdav
    
    Subscribers: eliwhite
    
    Differential Revision: https://phabricator.intern.facebook.com/D29247979
    
    Signature: 29247979:1624298790:799220e2539b589fd55c54aecaed21ccd98a6f09

--- vnext/Microsoft.ReactNative/Views/FlyoutViewManager.cpp
+++ vnext/Microsoft.ReactNative/Views/FlyoutViewManager.cpp
@@ -131,6 +131,7 @@ class FlyoutShadowNode : public ShadowNodeBase {
   winrt::Popup GetFlyoutParentPopup() const;
   winrt::FlyoutPresenter GetFlyoutPresenter() const;
   void OnShowFlyout();
+  void LogErrorAndClose(const string &error);
 
   xaml::FrameworkElement m_targetElement = nullptr;
   winrt::Flyout m_flyout = nullptr;
@@ -460,13 +461,27 @@ void FlyoutShadowNode::OnShowFlyout() {
         m_flyout.OverlayInputPassThroughElement(current);
       }
     }
-    m_flyout.ShowAt(m_targetElement, m_showOptions);
+
+    if (!m_targetElement && m_targetTag > 0) {
+      LogErrorAndClose("The target view unmounted before flyout could be shown.");
+    } else if (m_targetElement && m_flyout.XamlRoot() != m_targetElement.XamlRoot()) {
+      LogErrorAndClose("The target view window lost focus before flyout could be shown.");
+    } else {
+      m_flyout.ShowAt(m_targetElement, m_showOptions);
+    }
   } else {
     winrt::FlyoutBase::ShowAttachedFlyout(m_targetElement);
   }
 
   if (auto popup = GetFlyoutParentPopup()) {
     popup.IsLightDismissEnabled(m_isLightDismissEnabled);
+  }
+}
+
+void FlyoutShadowNode::LogErrorAndClose(const std::string &error) {
+  if (const auto instance = GetViewManager()->GetReactInstance().lock()) {
+    instance->CallJsFunction("RCTLog", "logToConsole", folly::dynamic::array("error", error));
+    OnFlyoutClosed(*instance, m_tag, false);
   }
 }
 
