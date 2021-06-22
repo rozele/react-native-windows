3ef4c7f5ac4e27bf8cd974715a12a615265ca5c6
Eric Rozell <erozell@outlook.com>
2021-06-22 00:25:30 -0400

    Adds undo/redo history to controlled changes

--- vnext/Microsoft.ReactNative/Views/TextInputViewManager.cpp
+++ vnext/Microsoft.ReactNative/Views/TextInputViewManager.cpp
@@ -38,6 +38,12 @@ struct Selection {
   int64_t start = -1;
   int64_t end = -1;
 };
+
+struct StringDiff {
+  size_t start;
+  size_t selectionLength;
+  winrt::hstring replacement;
+};
 } // namespace Microsoft::ReactNative
 
 // Such code is better to move to a seperate parser layer
@@ -133,6 +139,7 @@ class TextInputShadowNode : public ShadowNodeBase {
   void SetText(const winrt::Microsoft::ReactNative::JSValue &text);
   void SetSelection(int64_t start, int64_t end);
   winrt::Shape FindCaret(xaml::DependencyObject element);
+  static StringDiff DiffText(winrt::hstring const &oldValue, winrt::hstring const &newValue);
 
   bool m_initialUpdateComplete = false;
   bool m_autoFocus = false;
@@ -681,12 +688,18 @@ void TextInputShadowNode::SetText(const winrt::Microsoft::ReactNative::JSValue &
   if (m_mostRecentEventCount == m_nativeEventCount) {
     if (textBox) {
       if (text.Type() == winrt::Microsoft::ReactNative::JSValueType::String) {
-        auto oldCursor = textBox.SelectionStart();
-        auto oldSelectionLength = textBox.SelectionLength();
-        auto oldValue = textBox.Text();
-        auto newValue = asHstring(text);
+        const auto oldCursor = textBox.SelectionStart();
+        const auto oldSelectionLength = textBox.SelectionLength();
+        const auto oldValue = textBox.Text();
+        const auto newValue = asHstring(text);
         if (oldValue != newValue) {
-          textBox.Text(newValue);
+          if (!textBox.IsReadOnly()) {
+            const auto stringDiff = DiffText(oldValue, newValue);
+            textBox.Select(stringDiff.start, stringDiff.selectionLength);
+            textBox.SelectedText(stringDiff.replacement);
+          } else {
+            textBox.Text(newValue);
+          }
           if (oldValue.size() == newValue.size()) {
             textBox.SelectionStart(oldCursor);
           } else {
@@ -736,6 +749,25 @@ void TextInputShadowNode::dispatchCommand(
   } else {
     Super::dispatchCommand(commandId, std::move(commandArgs));
   }
+}
+
+StringDiff TextInputShadowNode::DiffText(winrt::hstring const &oldValue, winrt::hstring const &newValue) {
+  const auto textAdded = newValue.size() > oldValue.size();
+  // Find start index of string diff
+  size_t start = 0;
+  while (start < oldValue.size() && start < newValue.size() && oldValue[start] == newValue[start]) {
+    ++start;
+  }
+  // Find end index of string diff
+  size_t end = 1;
+  while (end <= oldValue.size() && end <= newValue.size() && oldValue[oldValue.size() - end] == newValue[newValue.size() - end]) {
+    ++end;
+  }
+  // Calculate selection length and replacement string
+  const auto selectionLength = oldValue.size() - end - start + 1;
+  const auto replacementLength = newValue.size() - end - start + 1;
+  const winrt::hstring replacement{newValue.begin() + start, replacementLength};
+  return {start, selectionLength, replacement};
 }
 
 TextInputViewManager::TextInputViewManager(const Mso::React::IReactContext &context) : Super(context) {}
